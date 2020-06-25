f6f01b51d6e0b8b768806e9f6a8c371859e349b3
Evaldo Ribeiro <evardis@gmail.com>
2020-06-25 19:14:19 -0300

    Projeto Equals alterado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,17 @@
 package application;
 
+import entities.Client;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
-		String a = "Maria";
-		String b = "Alex";
-		
-		System.out.println(a.equals(b));
 
+		Client c1 = new Client("Maria", "maria@gmail.com");
+		Client c2 = new Client("Maria", "alex@gmail.com");
+
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
 	}
 
 }
